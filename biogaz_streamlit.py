fd4eaa08fdad15beba5ed8f597800f469e4edb34
kemalk237 <kucukakkas@beyazpiramit.com.tr>
2025-04-28 14:28:55 +0300

    biogaz_streamlit.py güncellendi

---
+++ biogaz_streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Biyogaz Veri Paneli", layout="wide")
+
+st.title("Biyogaz Veri Paneli - Hoşgeldiniz 🚀")
+st.success("Streamlit kurulumu başarılı! Artık geliştirmeye başlayabiliriz.")
